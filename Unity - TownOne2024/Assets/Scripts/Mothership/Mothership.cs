26cceb789b9627e7ab485923cbbae71f12bedd1d
ItalianCannolis <61303396+ItalianCannolis@users.noreply.github.com>
2024-11-09 13:16:46 -0800

    Mothership Test Level Updates

--- Unity - TownOne2024/Assets/Scripts/Mothership/Mothership.cs
+++ Unity - TownOne2024/Assets/Scripts/Mothership/Mothership.cs
@@ -1,42 +1,93 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Mothership : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    float rotateZAxis = 0.5f;
+
     [SerializeField]
     float smooth = 5.0f;
+
     [SerializeField]
     float tiltAngle = 20.0f;
-    [SerializeField]
-    float rotateZAxis = 0.5f;
+
     [SerializeField]
     float turnAmount = 0.5f;
+
     [SerializeField]
+    public bool reverse;
+
+    [SerializeField]
+    public int sliceCount;
+
+    //[SerializeField]
     Transform trans;
 
     [SerializeField]
-    GameObject slices;
+    List<GameObject> slices = new List<GameObject>();
 
-
-
-    //float tiltAroundZ = 0f;
-    //float tiltAroundX = 0f;
-
-    //Quaternion target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
+    List<Quaternion> fortyFiveAngles = new List<Quaternion>();
     Quaternion target;
+
+    void Start()
+    {
+        trans = GetComponent<Transform>();
+        //Instantiate(slices[0], trans.position, Quaternion.identity);
+        SpawnSlices(sliceCount);
+    }
+
+    public bool Reverse
+    {
+        get { return reverse; }
+        set { reverse = value; }
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        rotateZAxis = rotateZAxis+turnAmount;
+        if (!reverse)
+        {
+            rotateZAxis = rotateZAxis + turnAmount;
+        }
+        else
+        {
+            rotateZAxis = rotateZAxis - turnAmount;
+        }
         //ChangeRotationTarget();
-            float tiltAroundZ = rotateZAxis * tiltAngle;
-            target = Quaternion.Euler(0, 0, tiltAroundZ);
+        float tiltAroundZ = rotateZAxis * tiltAngle;
+        target = Quaternion.Euler(0, 0, tiltAroundZ);
         // Dampen towards the target rotation
-        
-        trans.rotation = Quaternion.Slerp(transform.rotation, target,  Time.deltaTime * smooth);
+
+        trans.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
     }
 
+
+    void SpawnSlices(int count) { 
+
+
+        if (count == 4)
+        {
+            for(int i = 0; i < count; i++){
+            Instantiate(slices[0], trans.position, Quaternion.Euler(0, 0, (90*i)), trans);
+            }
+        }
+        else if(count == 3)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Instantiate(slices[0], trans.position, Quaternion.Euler(0, 0, (120 * i)), trans);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Instantiate(slices[0], trans.position, Quaternion.Euler(0, 0, (180 * i)), trans);
+            }
+        }
+    }
 
 
 }
