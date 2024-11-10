9fdfcf5008b78e76d1b57dc8c10162dbc833b354
nopetrides <nopetrides@gtmail.com>
2024-11-10 00:24:08 -0800

    settings menu fix

--- Unity - TownOne2024/Assets/Scripts/UI/Settings.cs
+++ Unity - TownOne2024/Assets/Scripts/UI/Settings.cs
@@ -1,14 +1,22 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Settings : MenuBase
 {
+    [SerializeField] private Button BackButton;
+
+    private void OnEnable()
+    {
+        BackButton.Select();
+    }
+
     public override GameMenus MenuType()
     {
         return GameMenus.SettingsMenu;
     }
 
-    public void MainMenu()
+    public void Close()
     {
-        UIMgr.Instance.ShowMenu(GameMenus.MainMenu);
+        UIMgr.Instance.HideMenu(GameMenus.SettingsMenu);
     }
 }
