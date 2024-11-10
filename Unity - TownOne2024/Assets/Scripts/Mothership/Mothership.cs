0bfc9d1c679dd9998cc3a9e0527b0382087ab080
ItalianCannolis <61303396+ItalianCannolis@users.noreply.github.com>
2024-11-09 16:03:45 -0800

    Clean up comments

--- Unity - TownOne2024/Assets/Scripts/Mothership/Mothership.cs
+++ Unity - TownOne2024/Assets/Scripts/Mothership/Mothership.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 
 public class Mothership : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     float rotateZAxis = 0.5f;
 
     [SerializeField]
@@ -23,7 +22,6 @@ public class Mothership : MonoBehaviour
     [SerializeField]
     public int containerTypeCount;
 
-    //[SerializeField]
     Transform trans;
 
     [SerializeField]
