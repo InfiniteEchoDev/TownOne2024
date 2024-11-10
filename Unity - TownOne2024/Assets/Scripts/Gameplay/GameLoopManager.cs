c5ebb7b24fb38bf53187882355228edd8c4e0d02
Kwiper <jonny.tibboh@gmail.com>
2024-11-09 16:47:37 -0800

    Added option for countdown timer in game loop.

--- Unity - TownOne2024/Assets/Scripts/Gameplay/GameLoopManager.cs
+++ Unity - TownOne2024/Assets/Scripts/Gameplay/GameLoopManager.cs
@@ -7,16 +7,37 @@ public class GameLoopManager : MonoBehaviour
     [SerializeField] float gameTimer;
     [SerializeField] int lives = 5;
 
+    [SerializeField] float maxTimer = 120f;
+    [SerializeField] bool isCountdownTimer;
+
     private void Start()
     {
         GameMgr.Instance.StartGame();
+
+        if (isCountdownTimer)
+        {
+            gameTimer = maxTimer;
+        }
+
     }
 
     private void Update()
     {
         if (GameMgr.Instance.IsGameRunning)
         {
-            gameTimer += 1 * Time.deltaTime; // Count up for now, may change later.
+            if (!isCountdownTimer)
+            {
+                gameTimer += 1 * Time.deltaTime; // Count up for now, may change later.
+            }
+            else
+            {
+                gameTimer -= 1 * Time.deltaTime;
+
+                if(gameTimer <= 0)
+                {
+                    GameMgr.Instance.NextLevel();
+                }
+            }
 
             if (lives <= 0)
             {
