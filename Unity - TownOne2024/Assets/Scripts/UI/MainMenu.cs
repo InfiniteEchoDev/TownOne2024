ded55b87465c29e8a99fc08202b93dc539bb5553
noahpetrides <nopetrides@gmail.com>
2024-11-09 17:16:38 -0800

    Button in main menu

--- Unity - TownOne2024/Assets/Scripts/UI/MainMenu.cs
+++ Unity - TownOne2024/Assets/Scripts/UI/MainMenu.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MenuBase
 {
+    [SerializeField] private Button _startButton;
     public override GameMenus MenuType()
     {
         return GameMenus.MainMenu;
+    }
+
+    private void OnEnable()
+    {
+        _startButton.Select();
     }
 }
