0093df13b8d5125f2dbb541fac890ac963edaa09
ItalianCannolis <61303396+ItalianCannolis@users.noreply.github.com>
2024-11-09 17:27:50 -0800

    Added collider to ship

---
+++ Unity - TownOne2024/Assets/Scripts/Pickups/ShipStorage.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections.Generic;
+public class ShipStorage : MonoBehaviour
+{
+
+    [SerializeField]
+    List<Pickup> pickups = new List<Pickup>();
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
