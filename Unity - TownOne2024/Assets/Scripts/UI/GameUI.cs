3b7670b19eee0f6ede20cb9cdcca652240aa4ce3
ItalianCannolis <61303396+ItalianCannolis@users.noreply.github.com>
2024-11-10 11:19:00 -0800

    Game UI Updates

--- Unity - TownOne2024/Assets/Scripts/UI/GameUI.cs
+++ Unity - TownOne2024/Assets/Scripts/UI/GameUI.cs
@@ -1,9 +1,44 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameUI : MenuBase
 {
+
+    [SerializeField]
+    TMP_Text displayLives;
+    [SerializeField]
+    TMP_Text displayScore;
+
+    GameLoopManager gameLoopManager;
     public override GameMenus MenuType()
     {
         return GameMenus.InGameUI;
     }
+
+    private void Start()
+    {
+        if (GameLoopManager.Instance == null) {
+            Debug.Log("GameloopManager not loaded in yet problems");
+        }
+        else if(GameLoopManager.Instance != null)
+        {
+            gameLoopManager = GameLoopManager.Instance;
+        }
+    }
+
+    private void Update()
+    {
+        if (GameLoopManager.Instance != null)
+        {
+            string livesCount;
+
+            livesCount = "x " + gameLoopManager.Lives;
+
+        }
+
+    }
+
+
 }
