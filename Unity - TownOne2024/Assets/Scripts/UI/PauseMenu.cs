f08ab0f3b284223b7a63ba3144cd51aacccecf52
Kwiper <jonny.tibboh@gmail.com>
2024-11-10 12:20:26 -0800

    Implemented pause menu
    + added pause menu UI
    + added button interactions to pause menu

--- Unity - TownOne2024/Assets/Scripts/UI/PauseMenu.cs
+++ Unity - TownOne2024/Assets/Scripts/UI/PauseMenu.cs
@@ -6,4 +6,14 @@ public class PauseMenu : MenuBase
     {
         return GameMenus.PauseMenu;
     }
+
+    public void MainMenu()
+    {
+        SceneMgr.Instance.LoadScene(GameScenes.MainMenu, GameMenus.MainMenu);
+    }
+
+    public void Resume()
+    {
+        GameMgr.Instance.UnpauseGame();
+    }
 }
