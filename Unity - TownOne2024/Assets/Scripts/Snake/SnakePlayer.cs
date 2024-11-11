411bd760675dd03b5b446eb1ef4dee4b169ca4a3
InfiniteEchoDev <79585979+InfiniteEchoDev@users.noreply.github.com>
2024-11-10 16:14:34 -0800

    Edge-turn movement, prevent moving backwards

--- Unity - TownOne2024/Assets/Scripts/Snake/SnakePlayer.cs
+++ Unity - TownOne2024/Assets/Scripts/Snake/SnakePlayer.cs
@@ -1,7 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
 using UnityEngine;
+
 
 public class SnakePlayer : MonoBehaviour
 {
+
     [Header( "Obj Refs" )]
     public SpriteRenderer _shipSprite;
     public GameObject _attachPoint;
@@ -13,6 +19,7 @@ public class SnakePlayer : MonoBehaviour
     private Vector2 _playerPosition = Vector2.zero;
     private Vector2 _previousPosition = Vector2.zero;
 
+    private Vector2Int _previousCoordinates;
     private Vector2Int _coordinates;
     
     // Private Components
@@ -33,12 +40,13 @@ public class SnakePlayer : MonoBehaviour
         if (xInput > _deadZone)
         {
             // Right move
-            _lastInput = CardinalDirection.East;
-        }
-        else if (xInput < -_deadZone)
+            if( _lastInput != CardinalDirection.West )
+                _lastInput = CardinalDirection.East;
+        } else if (xInput < -_deadZone)
         {
             // Left Move
-            _lastInput = CardinalDirection.West;
+            if( _lastInput != CardinalDirection.East )
+                _lastInput = CardinalDirection.West;
         }
     }
 
@@ -47,11 +55,12 @@ public class SnakePlayer : MonoBehaviour
         if (yInput > _deadZone)
         {
             // Right move
-            _lastInput = CardinalDirection.North;
-        }
-        else if (yInput < -_deadZone)
+            if( _lastInput != CardinalDirection.South )
+                _lastInput = CardinalDirection.North;
+        } else if (yInput < -_deadZone)
         {
             // Left Move
+            if( _lastInput != CardinalDirection.North )
             _lastInput = CardinalDirection.South;
         }
     }
@@ -59,16 +68,21 @@ public class SnakePlayer : MonoBehaviour
     private void MovePlayerWithTimer()
     {
         _previousDirection = _lastInput;
-        var previousCoords = _coordinates;
+        var currentCoords = _coordinates;
         
-        var nextPos = _snakeGrid.NextPositionInDirection(_lastInput, _coordinates);
-        transform.position = new Vector3(nextPos.x * _snakeGrid.CellWidth, nextPos.y * _snakeGrid.CellHeight, 0);
-        _coordinates = nextPos;
-        
-        // todo
-        // if (snakebody.any)
-        // firstSnakebody.OnGridMove(previousCoords)
-        
+        var nextPos = _snakeGrid.NextValidPositionInDirection(_lastInput, _coordinates);
+        if( nextPos == null ) {
+            var validPosList = _snakeGrid.GetAllValidMovementPosFromPos( _coordinates );
+            validPosList.Remove( validPosList.First( validPos => validPos.pos == _previousCoordinates ) );
+
+            int randIdx = Random.Range( 0, validPosList.Count );
+            _lastInput = validPosList[randIdx].dir;
+            nextPos = validPosList[randIdx].pos;
+        }
+
+        transform.position = new Vector3(nextPos.Value.x * _snakeGrid.CellWidth, nextPos.Value.y * _snakeGrid.CellHeight, 0);
+        _previousCoordinates = _coordinates;
+        _coordinates = nextPos.Value;
     }
     
 }
